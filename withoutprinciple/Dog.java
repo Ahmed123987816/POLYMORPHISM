40b3431f0a897f8237bd83c4765397bd83ad8b4b
Ahmed123987816 <ahmadnawaz3354@gmail.com>
2025-03-16 20:41:01 +0500

    Add files via upload

---
+++ withoutprinciple/Dog.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package withoutprinciple;
+
+class Dog {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Class Cat
+class Cat {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
---
+++ withoutprinciple/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package withoutprinciple;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.sound(); // Calling Dog sound
+        cat.sound(); // Calling Cat sound
+    }
+}
